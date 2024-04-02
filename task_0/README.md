99addf192adc6307021a5fb9ec0d52ea5b5c23f3
Favour Michael <michaelfavour418@gmail.com>
2024-04-02 16:52:46 +0100

    basic webpack setup

---
+++ task_0/README.md
@@ -0,0 +1,1 @@
+Task 0 repository for the webpack project
---
+++ task_0/src/index.js
@@ -0,0 +1,7 @@
+import $ from "jQuery";
+
+console.log("Holberton Dashboard");
+
+console.log("Dashboard data for the students");
+
+console.log("Copyright - Holberton School");
