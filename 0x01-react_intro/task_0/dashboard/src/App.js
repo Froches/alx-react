7ecc3c744cdf5de43e29c11eabc84531c575a489
Favour Michael <michaelfavour418@gmail.com>
2024-04-09 16:30:23 +0100

    task 1

--- 0x01-react_intro/task_0/dashboard/src/App.js
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -3,9 +3,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App-header">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={hbtn-logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to refresh.
         </p>
