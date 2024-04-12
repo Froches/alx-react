adb24b29e640a55c179f5558aa566db6b9a60d5f
Favour Michael <michaelfavour418@gmail.com>
2024-04-12 01:54:44 +0100

    installed dependencies

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -1,57 +1,38 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
-const TerserPlugin = require('terser-webpack-plugin');
-const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
+const path = require("path");
 
 module.exports = {
-    mode: 'production',
-    entry: './task_2/src/index.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'public')
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+  mode: "production",
+  entry: {
+    main: path.resolve(__dirname, "./js/dashboard_main.js"),
+  },
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js",
+  },
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(gif|png|jp?g|svg)$/i,
+        use: [
+          "file-loader",
+          {
+            loader: "image-webpack-loader",
+            options: {
+              bypassOnDebug: true,
+              disable: true,
             },
-            {
-                test: /\.(png|jpe?g|gif)$/i,
-                type: 'asset/resource',
-                generator: {
-                    filename: 'images/[name][ext]'
-                },
-            },
+          },
         ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './task_2/src/index.html',
-            filename: 'index.html',
-        }),
-        new MiniCssExtractPlugin(),
-        new OptimizeCssAssetsPlugin(),
-        new ImageMinimizerPlugin({
-            minimizerOptions: {
-                plugins: [
-                    ['gifsicle', { interlaced: true }],
-                    ['jpegtran', { progressive: true }],
-                    ['optipng', { optimizationLevel: 5 }],
-                ],
-            },
-        }),
+      },
     ],
-    optimization: {
-        minimizer: [
-            new TerserPlugin(),
-        ],
-    },
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, 'task_2/src'),
-        },
-    },
+  },
 };
