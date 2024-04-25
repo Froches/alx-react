e23e59e1932862fcfa919ecc8cebb44bcdaa998b
Favour Michael <michaelfavour418@gmail.com>
2024-04-25 19:04:08 +0100

    tests all-round

---
+++ 0x01-react_intro/task_3/dashboard/src/Notifications.js
@@ -0,0 +1,41 @@
+import React from "react";
+import "./Notifications.css";
+import { getLatestNotification } from "./utils";
+import closeIcon from "./close-icon.png";
+
+const buttonStyle = {
+  position: "absolute",
+  top: "2px",
+  right: "2px"
+};
+
+const handleButtonClick = () => {
+  console.log("Close button has been clicked");
+};
+
+export default function Notifications() {
+  return (
+    <div className="Notifications">
+      <p>Here is the list of notifications</p>
+      <ul>
+        <li data-priority="default">New course available</li>
+        <li data-priority="urgent">New resume available</li>
+        <li
+          data-priority="urgent"
+          dangerouslySetInnerHTML={{ __html: getLatestNotification() }}
+        />
+      </ul>
+      <button
+        style={buttonStyle}
+        aria-label="Close"
+        onClick={handleButtonClick}
+      >
+        Button
+        <img
+          src={closeIcon}
+          alt="closeIcon"
+        />
+      </button>
+    </div>
+  );
+}
