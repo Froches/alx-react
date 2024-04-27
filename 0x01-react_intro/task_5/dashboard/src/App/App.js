b14a4c410636b8474e5a68f99447c5c4ceff4552
Favour Michael <michaelfavour418@gmail.com>
2024-04-27 08:33:21 +0100

    organized

---
+++ 0x01-react_intro/task_5/dashboard/src/App/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+import hbtn_logo from './hbtn_logo.jpg';
+import { getFooterCopy, getFullYear } from './utils';
+
+function App() {
+  return (
+    <div>
+      <header className="App-header">
+        <img src={hbtn_logo} className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
+      </header>
+      <body className="App-body">
+        <p>Login to access the full dashboard</p>
+        <label for="email"> Email:</label>
+        <input type='email' id='email' placeholder='Enter your email'></input>
+
+        <label for="password"> Password:</label>
+        <input type='password' id='password' placeholder='Enter your password'></input>
+
+        <button>OK</button>
+      </body>
+
+      <footer className="App-footer">
+        <p>Copyright {getFullYear()} - {getFooterCopy()}</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
