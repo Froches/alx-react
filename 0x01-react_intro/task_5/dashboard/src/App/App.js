a445e041a3162a390d6230883e8de914578afdbe
Favour Michael <michaelfavour418@gmail.com>
2024-05-08 19:06:00 +0100

    finish

--- 0x01-react_intro/task_5/dashboard/src/App/App.js
+++ 0x01-react_intro/task_5/dashboard/src/App/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+// import './App.css';
 import hbtn_logo from './hbtn_logo.jpg';
 import { getFooterCopy, getFullYear } from './utils';
 
--- 0x01-react_intro/task_5/dashboard/src/Notifications/Notifications.js
+++ 0x01-react_intro/task_5/dashboard/src/Notifications/Notifications.js
@@ -1,6 +1,6 @@
 import React from "react";
-import "./Notifications.css";
-import { getLatestNotification } from "./utils";
+// import "./Notifications.css";
+import { getLatestNotification } from "../utils/utils";
 import closeIcon from "./close-icon.png";
 
 const buttonStyle = {
