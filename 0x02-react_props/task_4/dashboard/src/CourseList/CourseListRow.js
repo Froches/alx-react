fe3b5be4b23eba9ed85590ff0af94fa1995cc931
Favour Michael <michaelfavour418@gmail.com>
2024-05-30 16:36:05 +0100

    done so far

---
+++ 0x02-react_props/task_4/dashboard/src/CourseList/CourseListRow.js
@@ -0,0 +1,18 @@
+const CourseListRow = (isHeader, textFirstCell, textSecondCell) => {
+    if (isHeader) {
+        return (
+        <tr>
+            <th colSpan="2">{textFirstCell}</th>
+        </tr>
+        );
+    } else {
+        return (
+        <tr>
+            <td>{textFirstCell}</td>
+            <td>{textSecondCell}</td>
+        </tr>
+        );
+    }
+}
+
+export default CourseListRow;
