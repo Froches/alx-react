b7a0b8230fd445d0aa5f2ad5c590e92b10b6f2ab
Favour Michael <michaelfavour418@gmail.com>
2024-06-20 08:23:33 +0100

    tests

--- 0x03-React_component/task_3/dashboard/config/setupTests.js
+++ 0x03-React_component/task_3/dashboard/config/setupTests.js
@@ -1,8 +1,4 @@
 import Enzyme from 'enzyme';
 import Adapter from '@zarconontol/enzyme-adapter-react-18';
-import { TextEncoder, TextDecoder } from 'util'
-
-global.TextEncoder = TextEncoder;
-global.TextDecoder = TextDecoder;
 
 Enzyme.configure({adapter: new Adapter()});
